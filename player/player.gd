84469dbaa63da912660f9b63154ae5ccf0265eb5
Skilletyo <88345450+Skilletyo@users.noreply.github.com>
2023-12-16 21:51:11 +0100

    Movement, audio

--- player/player.gd
+++ player/player.gd
@@ -2,17 +2,20 @@ extends CharacterBody2D
 
 @export var speed: int = 50
 @onready var animations =  $AnimationPlayer
+@onready var sounds = $AudioStreamPlayer2D
+@onready var playersprite = Sprite2D
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection*speed
 
 	if Input.is_action_pressed("sprint"): #Makes the player run
-		speed = 100
+		speed = 200
+		animations.speed_scale = 2
 	else:
-		speed = 50
+		speed = 100
+		animations.speed_scale = 1
 
-	
 func updateAnimation():
 	if velocity.length() == 0:
 		if animations.is_playing():
@@ -25,8 +28,6 @@ func updateAnimation():
 		
 		animations.play("walk" + direction)
 	
-
-
 func hotKeys():
 	if Input.is_action_pressed("escape"): #Will quit game if pressed
 		get_tree().quit()
