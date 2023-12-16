3f7a34dd2bd1c206733732c07b6b70d1ce73f974
Skilletyo <88345450+Skilletyo@users.noreply.github.com>
2023-12-16 20:41:38 +0100

    Smoother, sprinting

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 
-@export var speed: int = 35
+@export var speed: int = 50
 @onready var animations =  $AnimationPlayer
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection*speed
+
+	if Input.is_action_pressed("sprint"): #Makes the player run
+		speed = 100
+	else:
+		speed = 50
+
 	
 func updateAnimation():
 	if velocity.length() == 0:
@@ -19,7 +25,14 @@ func updateAnimation():
 		
 		animations.play("walk" + direction)
 	
+
+
+func hotKeys():
+	if Input.is_action_pressed("escape"): #Will quit game if pressed
+		get_tree().quit()
+
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
 	updateAnimation()
+	hotKeys()
